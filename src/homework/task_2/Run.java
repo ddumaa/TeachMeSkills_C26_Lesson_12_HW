ee285227e4efd7e5c2403c40e8a269f29f7acd60
ddumaa <dimaduma62@gmail.com>
2023-12-11 14:48:46 +0300

    fix

--- src/homework/task_2/Run.java
+++ src/homework/task_2/Run.java
@@ -14,10 +14,11 @@ import homework.task_2.utils.SearchPhoneNumber;
 
 public class Run {
     public static void main(String[] args) {
-        String input = "The program receives arbitrary text as input. This text may contain a number\n" +
-                "document (one or more), email and phone number. Document number in format:\n" +
-                "1423-1512-51, where x is any number; telephone number in the format: +(23)1234567. Document\n" +
-                "may contain email teachmeskills@gmail.com";
+        String input = """
+                The program receives arbitrary text as input. This text may contain a number
+                document (one or more), email and phone number. Document number in format:
+                1423-1512-51, where x is any number; telephone number in the format: +(23)1234567. Document
+                may contain email teachmeskills@gmail.com""";
         DocumentSearch.documentSearch(input);
         SearchPhoneNumber.searchPhoneNumber(input);
         SearchEmail.searchEmail(input);
